985ab2ab48a7f19ef34ebf3da6bbda744cbd07e9
shimizuryoto <ryoto.s.0524@gmail.com>
2024-03-05 12:36:22 +0900

    無駄を省略+コメント修正

--- index.js
+++ index.js
@@ -2,28 +2,31 @@ const form = document.getElementById("form");
 const input = document.getElementById("input");//ホームに入力されたinputタグのデータを入れる
 const ul = document.getElementById("ul");
 const ulAll = document.getElementById("ulAll");
-let Alllist = JSON.parse(localStorage.getItem("Alllist"));
+let Alllist = JSON.parse(localStorage.getItem("Alllist"));//ローカルストレージを呼び出してる
 
+//現在の時間の取得
 const currentDate = new Date();
-const thisyear = currentDate.getFullYear(); // 年を取得
-const thismonth = String(currentDate.getMonth() + 1).padStart(2, '0'); // 月を取得（0埋め）
-const today = String(currentDate.getDate()).padStart(2, '0'); // 日を取得（0埋め）
+const thisYear = currentDate.getFullYear();
+const thisMonth = currentDate.getMonth() + 1;
+const toDay = currentDate.getDate();
+const month = String(thisMonth).padStart(2, '0');
+const day = String(toDay).padStart(2, '0');
+
 
 
 // ページが読み込まれたときに日付を挿入する
 window.addEventListener('load', function() {
     const dateElement = document.getElementById('date');
-    const currentDate1 = `${thisyear}/${thismonth}/${today}`;
+    const currentDate1 = `${thisYear}/${month}/${day}`;
     dateElement.textContent = ` ${currentDate1}`;
 });
 
 //フォーム入力
 form.addEventListener("submit", function(event){//enterで反応
-    //event.preventDefault();//リロード阻止
     add();
 });
 
-//配列にあるやつをリロードしても入れる機能
+//リロードしても入れる機能
 if (Alllist){
     Alllist.forEach(item => {
         display(item);
@@ -33,12 +36,8 @@ if (Alllist){
 
 
 function add() {
-    // ローカルストレージから既存のデータを取得
-    let existingData = localStorage.getItem("Alllist");
-
-    // 既存のデータがある場合はパースして配列に変換し、ない場合は空の配列を生成
-    let allData = existingData ? JSON.parse(existingData) : [];
-
+    //空の場合は入れない
+    if (input.value.length > 0){
     let data = {//オブジェクト
         text: input.value,
         completed: false,
@@ -47,26 +46,26 @@ function add() {
         status:0,
         id: "todoId"
     };
-    allData.push(data);
-    localStorage.setItem("Alllist", JSON.stringify(allData));
+    Alllist.push(data);
+    localStorage.setItem("Alllist", JSON.stringify(Alllist));
     input.value = "";
+    }
 }
 
 function display(item) {
     //todoの更新
     check(item);
 
-    //li準備
-    const li = document.createElement("li");//li要素の定義
-    li.innerText = item.text;//テキストを入れてる
-    li.classList.add("list-group-item");//liにlist-group-itemというクラス名を追加
+    //li要素の定義
+    const li = document.createElement("li");
+    li.innerText = item.text;
+    li.classList.add("list-group-item");
     li.id = item.id
 
-    //横線ひくか否か
+    //左クリック横線
     if (item && item.completed){
         li.classList.add("text-decoration-line-through");
     }
-    //左クリック横線
     li.addEventListener("click", function(event){
         li.classList.toggle("text-decoration-line-through");
         addsaveData(item);
@@ -88,17 +87,15 @@ function display(item) {
     addsaveData(item);
 }
 
+//ローカルストレージから削除してる
 function removeData(text) {
     Alllist = Alllist.filter(item => item.text !== text);
     localStorage.setItem("Alllist", JSON.stringify(Alllist));
 }
 
-function check(item) {//ここでstatusを変えてる．
-
+///statusを変える．
+function check(item) {
     let updateNeeded = false;
-    const year = currentDate.getFullYear(); // 年を取得
-    const todayMonth = currentDate.getMonth() + 1;
-    const todayDay = currentDate.getDate();
     const specialmonthes = [4, 6, 9, 11];
     let dec = 0;
     let adjust = 0;
@@ -106,7 +103,7 @@ function check(item) {//ここでstatusを変えてる．
     if (specialmonthes.includes(item.month)) {
         adjust = 1;
     } else if (item.month === 2) {
-        if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)){//うるう年判定
+        if ((thisYear % 4 === 0 && thisYear % 100 !== 0) || (thisYear % 400 === 0)){//うるう年判定
             adjust = 2;
         } else{
             adjust = 3;
@@ -116,43 +113,42 @@ function check(item) {//ここでstatusを変えてる．
     }   
 
     // その他は31日/月
-    if (item.status === 0 && item.day !== todayDay) {
+    if (item.status === 0 && item.day !== toDay) {
         updateNeeded = true;
-    } else if (item.status === 1 && (item.day === (todayDay - 3) || (item.day >= (29-adjust) && item.day === (todayDay - 3 + 31-adjust)))) {
+    } else if (item.status === 1 && (item.day === (toDay - 3) || (item.day >= (29-adjust) && item.day === (toDay - 3 + 31-adjust)))) {
         updateNeeded = true;
-    } else if (item.status === 2 && (item.day === (todayDay - 7) || (item.day >= (25-adjust) && item.day === (todayDay - 7 + 31-adjust)))) {
+    } else if (item.status === 2 && (item.day === (toDay - 7) || (item.day >= (25-adjust) && item.day === (toDay - 7 + 31-adjust)))) {
         updateNeeded = true;
-    } else if (item.status === 3 && (item.day === (todayDay - 14) || (item.day >= (18-adjust) && item.day === (todayDay - 14 + 31-adjust)))) {
+    } else if (item.status === 3 && (item.day === (toDay - 14) || (item.day >= (18-adjust) && item.day === (toDay - 14 + 31-adjust)))) {
         updateNeeded = true;
-    } else if (item.status === 4 && item.month === (todayMonth - 1 + dec)) {
+    } else if (item.status === 4 && item.month === (thisMonth - 1 + dec)) {
         updateNeeded = true;
     }
 
     if (updateNeeded) {
-        item.month = currentDate.getMonth() + 1;
-        item.day = currentDate.getDate();
+        item.month = thisMonth;
+        item.day = toDay;
         item.status++;
     }
 
-    if (item.month === currentDate.getMonth() + 1 && item.day === currentDate.getDate()) {
+    if (item.month === thisMonth && item.day === toDay) {
         item.id = "todoId";
     } else {
         item.id = "AllId";
     }
 }
 
+//データをセーブ
 function addsaveData(item) {
-    let existingData = JSON.parse(localStorage.getItem("Alllist")) || []; // 既存のデータを取得
-
     // 重複をチェック
-    let isDuplicate = existingData.some(list => {
+    let isDuplicate = Alllist.some(list => {
         return list.text === item.text && list.month === item.month && list.day === item.day && list.status === item.status;
     });
 
     if (!isDuplicate) {
-        existingData = existingData.filter(list => list.text !== item.text); // 更新前のリストを削除
-        existingData.push(item);
-        localStorage.setItem("Alllist", JSON.stringify(existingData)); // 保存
+        Alllist = Alllist.filter(list => list.text !== item.text); // 更新前のリストを削除
+        Alllist.push(item);
+        localStorage.setItem("Alllist", JSON.stringify(Alllist)); // 保存
     }
 }
 
